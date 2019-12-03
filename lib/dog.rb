c917dfe46300559e30f01147409377451f1df2f9
wolfene2001 <wolfene2001@yahoo.com>
2019-12-03 21:21:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   def name=(dogs_name)
     @this_dogs_name = dogs_name
   end
-  
+
   def name
     @this_dogs_name
   end
@@ -10,5 +10,5 @@ end
 
   lassie = Dog.new
   lassie.name = "Lassie"
-  
+
   puts lassie.name
